39faae1db58532c529b3db84f0b7ededbc8fa786
phlmox <62145317+phlmox@users.noreply.github.com>
2022-03-25 23:53:54 +0300

    Update urlgod.py

--- urlgod.py
+++ urlgod.py
@@ -5,56 +5,37 @@ import re,json,requests,time,threading
 from urllib.parse import urlparse
 
 class Service:
-    def ccFetch(self,ind,last,domain,pages):
-        for c in pages[ind:last]:
-            try:
-                r = requests.get(f"https://index.commoncrawl.org/{c}-index?url=*.{domain}/*&output=json")
-                data = [json.loads(x) for x in r.text.strip().split('\n')]
-                u=[x["url"] for x in data]
-                Log(f"Found {str(len(u))} urls from index {c}, Service: 'CommonCrawl'","inf")
-                self.CCUrls+= u
-            except KeyboardInterrupt:
-                exit()
-            except:
-                Log(f"Couldn't find any urls from {c}","err")
-        self.wait+=1
+    def ccFetch(self,domain,page):
+        Log(f"Fetching urls from {page}","inf")
+        r = requests.get(f"https://index.commoncrawl.org/{page}-index?url=*.{domain}/*&output=json")
+        data = [json.loads(x) for x in r.text.strip().split('\n')]
+        [print(x["url"],flush=True) for x in data]
 
     def getCommonCrawlURLs(self,domain):
         try:
             CCS=[(x["id"]) for x in json.loads(requests.get("http://index.commoncrawl.org/collinfo.json").content)]
             if self.quick:
-                self.ccFetch(0,1,domain,CCS)
+                self.ccFetch(domain,CCS[0])
             else:
-                self.wait=0
-                tPer = len(CCS)//self.CCThreads
-                tLeft = len(CCS)-(tPer*self.CCThreads)
-                tIndex = 0
-                for t in range(self.CCThreads):
-                    last=tIndex+tPer
-                    if t==self.CCThreads-1:
-                        last+=tLeft
-
-                    threading.Thread(target=self.ccFetch,args=(tIndex,last,domain,CCS)).start()
-                    tIndex+=tPer
-                while self.wait!=self.CCThreads:
-                    pass
-            Log(f"Found total {str(len(self.CCUrls))} urls from 'CommonCrawl'","inf")
-            return self.CCUrls
+                for t in range(len(CCS)):
+                    time.sleep(10)
+                    threading.Thread(target=self.ccFetch,args=(domain,CCS[t])).start()
         except KeyboardInterrupt:
             exit()
+        except:
+            pass
+
     def getVirusTotalURLs(self,domain,apikey):
-        urls=[]
         while 1:
             try:
                 Log(f"Trying to collect urls from 'VirusTotal'","ok")
                 r = requests.get(f"https://www.virustotal.com/vtapi/v2/domain/report?apikey={apikey}&domain={domain}")
                 data = json.loads(r.text)
                 for u in data["undetected_urls"]:
-                    urls.append(u[0])
+                    print(u[0])
                 for u in data["detected_urls"]:
-                    urls.append(u["url"])
-                Log(f"Found total {str(len(urls))} urls from 'VirusTotal'","inf")
-                return urls
+                    print(u["url"],flush=True)
+                return
             except KeyboardInterrupt:
                 exit()
             except json.decoder.JSONDecodeError:
@@ -64,23 +45,19 @@ class Service:
     def getWaybackURLs(self,domain):
         try:
             url = f"https://web.archive.org/cdx/search/cdx?url=*.{domain}/*&output=json&&collapse=urlkey"
-            data=json.loads(requests.get(url).text)
-            urls=[x[2] for x in data]
-            Log(f"Found total {str(len(urls))} urls from 'WebArchive'","inf")
-            return urls
+            data=json.loads(requests.get(url).text)[1:]
+            [print(x[2],flush=True) for x in data]
         except KeyboardInterrupt:
             exit()
         except:
             Log(f"Error with 'WebArchive'","err")
-            return []
 
     def otxFetch(self,ind,last,domain,pages):
         for p in range(ind,last):
             data=json.loads(requests.get(f"https://otx.alienvault.com/api/v1/indicators/domain/{domain}/url_list?limit=500&page={str(p+1)}").text)
-            u=[data["url_list"][i]["url"] for i in range(len(data["url_list"]))]
+            [print(data["url_list"][i]["url"],flush=True) for i in range(len(data["url_list"]))]
             self.OTXCollected+=1
             Log(f"Collected {str(self.OTXCollected)}/{str(pages)} from 'OTX'","inf")
-            self.OTXUrls +=u
         self.wait+=1
 
     def getOTXURLs(self,domain):
@@ -88,8 +65,7 @@ class Service:
             data=json.loads(requests.get(f"https://otx.alienvault.com/api/v1/indicators/domain/{domain}/url_list?limit=500&page=1").text)
             full_size = data["full_size"]
             if full_size < 500:
-                Log(f"Found total {str(full_size)} urls from 'OTX'","inf")
-                return [data["url_list"][i]["url"] for i in range(full_size)]
+                [print(x,flush=True) for x in [data["url_list"][i]["url"] for i in range(full_size)]]
             else:
                 left=full_size%500
                 pages=(full_size-left)//500 if left==0 else (full_size-left)//500+1
@@ -107,40 +83,28 @@ class Service:
                     tIndex+=tPer
                 while self.wait!=self.OTXThreads:
                     pass
-                Log(f"Found total {str(len(self.OTXUrls))} urls from 'OTX'","inf")
-                return self.OTXUrls
         except KeyboardInterrupt:
             exit()
         except Exception as e:
-            print(e)
             Log(f"Error with 'OTX'","err")
-            return []
 
     def Get(self,domain):
-        uri=[]
         Log(f"Collecting urls from 'WebArchive'","ok")
-        o=self.getWaybackURLs(domain)
-        uri+=o
+        self.getWaybackURLs(domain)
         Log(f"Collecting urls from 'CommonCrawl'","ok")
-        o=self.getCommonCrawlURLs(domain)
-        uri+=o
+        self.getCommonCrawlURLs(domain)
         if self.virustotal != None:
             Log(f"Collecting urls from 'VirusTotal'","ok")
-            o=self.getVirusTotalURLs(domain,self.virustotal)
-            uri+=o
+            self.getVirusTotalURLs(domain,self.virustotal)
         Log(f"Collecting urls from 'OTX'","ok")
-        o=self.getOTXURLs(domain)
-        uri+=o
-        return uri
+        self.getOTXURLs(domain)
+        return 1
     
-    def __init__(self,quick,virustotal):
+    def __init__(self,quick,threads,virustotal):
         self.quick = quick
         self.virustotal = virustotal
-        self.OTXThreads = 5
-        self.OTXUrls = []
+        self.OTXThreads = threads
         self.OTXCollected = 0
-        self.CCThreads = 10
-        self.CCUrls = []
         self.CCCollected = 0
         self.wait = 0
 
@@ -175,17 +139,17 @@ def print_banner():
                 __/ |            
                |___/             
                
-                    v2.0 | by @EnesSaltk7\n"""
+                    v1.0 | by @EnesSaltk7\n"""
     print(banner, file=sys.stderr)
 
 
 parser = argparse.ArgumentParser(description='urlgod - Fetches URLs from various services.')
 
 parser.add_argument('-d', '--domain', help="Domain")
-parser.add_argument('-o', '--output', required=False, help="Output filename")
 parser.add_argument('-v', '--virustotal', required=False, help="Virustotal API Key")
 parser.add_argument('-q', '--quick', action='store_true', help="Quick scan")
 parser.add_argument('-s', '--silent', action='store_true', help="Silent mode")
+parser.add_argument('-t', '--threads', default=5,help="Default 5")
 
 if len(sys.argv)<2:
     print_banner()
@@ -197,12 +161,5 @@ args = parser.parse_args()
 if not args.silent:
     print_banner()
 Log("Fetching urls for "+args.domain,"ok")
-o=Service(args.quick,args.virustotal).Get(args.domain)
-Log(f"Final urls: {len(o)}","ok")
-if args.output!=None:
-    with open(args.output, 'w') as f:
-        for item in o:
-            f.write("%s\n" % item)
-
-[print(x) for x in o]
+o=Service(args.quick,int(args.threads),args.virustotal).Get(args.domain)
 Log("Good luck haxor!","ok")
